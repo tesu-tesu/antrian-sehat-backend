d7f0981722ea50d25190ae9f8b0c37c08e4321ef
Yanu Adi Nugraha <2110181004@msoffice.pens.ac.id>
2020-10-11 15:32:02 +0700

    define constant, change migration name order, and fill migration

---
+++ app/Constant.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author yanuadin <yanu30adi@gmail.com>
+ * at 10/11/20 , 7:14 AM
+ */
+
+define('PATIENT_STATUS', ['Belum Diperiksa', 'Sedang Diperiksa', 'Sudah Diperiksa', 'Dibatalkan']);
+define('ROLE', ['Admin', 'Super Admin', 'Pasien']);
+define('DAY', ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']);
