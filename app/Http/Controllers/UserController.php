65f6aa4352748aafb0c1e45fa639b25ba29d7cc6
satriyoaji <satriyoaji27@gmail.com>
2020-10-13 20:03:19 +0700

    User Controller Resource worked

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -39,7 +40,34 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:3,150',
+            'email' => 'required|string|email|unique:users|max:100',
+            'password' => 'required|string|min:6',
+            'phone' => 'required|numeric|digits_between:8,13',
+            'role' => 'required|string',
+            'residence_number' => 'nullable|numeric|unique:users|digits:16',
+            'health_agency' => 'nullable|numeric'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role' => $request->role,
+            'residence_number' => $request->residence_number,
+            'health_agency_id' => $request->health_agency,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'new User has successfully created',
+            'user' => $user
+        ], 200);
     }
 
     /**
@@ -74,7 +102,49 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $data = User::find($user->id);
+
+        if (!$data) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User data not found'
+            ], 400);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:3,150',
+            'email' => 'required|string|email|unique:users,email,' .$user->id. '|max:100',
+            'password' => 'required|string|min:6',
+            'phone' => 'required|numeric|digits_between:8,13',
+            'role' => 'required|string',
+            'residence_number' => 'nullable|numeric|unique:users,residence_number,' .$user->id. '|digits:16',
+            'health_agency' => 'nullable|numeric'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $updated = User::where('id', $user->id)
+            ->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'role' => $request->role,
+                'residence_number' => $request->residence_number,
+                'health_agency_id' => $request->health_agency,
+                'password' => bcrypt($request->password)
+            ]);
+
+        if ($updated)
+            return response()->json([
+                'success' => true,
+                'message' => 'User data updated successfully!'
+            ], 200);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'User data can not be updated'
+            ], 500);
     }
 
     /**
@@ -85,6 +155,25 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $data = User::find($user->id);
+ 
+        if (!$data) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User data not found'
+            ], 400);
+        }
+ 
+        if ($data->delete()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'User has successfully deleted'
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'User can not be deleted'
+            ], 500);
+        }
     }
 }
