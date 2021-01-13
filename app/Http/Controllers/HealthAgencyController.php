3c8506ce941e9aee184241928befe1edf56bfd6a
satriyoaji <satriyoaji27@gmail.com>
2021-01-14 01:48:01 +0700

    Add simple pagination for search HA results Fixes #212

--- app/Http/Controllers/HealthAgencyController.php
+++ app/Http/Controllers/HealthAgencyController.php
@@ -214,20 +214,13 @@ class HealthAgencyController extends Controller
 
         $results = [];
         if ($search != null) {
-            $query1 = HealthAgency::where('name', 'like', '%' . $search . '%')->get();
-            $query2 = PolyMaster::where('name', 'like', '%' . $search . '%')
-                ->with('polyclinics', 'polyclinics.health_agency')->get();
+            $results['name']  = HealthAgency::where('name', 'like', '%' . $search . '%')->simplePaginate(5);
 
-            foreach ($query1 as $item) {
-                $results['name'][] = $item;
-            }
-            foreach ($query2 as $polymaster) {
-                foreach ($polymaster->polyclinics as $polyclinic) {
-                    if (!in_array($polyclinic->health_agency, $results)) {
-                        $results['contains'][] = $polyclinic->health_agency;
-                    }
-                }
-            }
+            $results['contains'] = HealthAgency::whereHas('polyclinics', function ($q) use ($search) {
+                $q->whereHas('poly_master', function ($q) use ($search) {
+                    $q->where('name', 'like', '%' . $search . '%');
+                });
+            })->simplePaginate(5);
         }
 
         $data = $results;
