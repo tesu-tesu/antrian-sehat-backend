47ac618f2033951b8e27609d8bc9ac6cc94130af
yanuadin <43435281+yanuadin@users.noreply.github.com>
2021-01-15 21:40:48 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PolyMasterSeeder::class);
-        $this->call(IconPolyMasterSeeder::class);
         $this->call(HealthAgencySeeder::class);
         $this->call(UserSeeder::class);
     }
